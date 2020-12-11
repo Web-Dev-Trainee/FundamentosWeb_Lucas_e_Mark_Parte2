78cdb82aae9d5ed5772f3e382b8e303897a2f174
Lucas Souza Leão <lucaleaoo2019@gmail.com>
2020-12-11 09:36:05 -0300

    Update Fundamentos

---
+++ exer01/troubleshooting.js
@@ -0,0 +1,13 @@
+function troubleshooting() {
+	const a = 1;
+	const b = 1;
+
+	// Edite essas linhas
+	// =================================
+	return a + b;
+
+	// =================================
+}
+
+// Do not change this 
+module.exports = troubleshooting;
